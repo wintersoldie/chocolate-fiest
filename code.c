5d7c86a50986891073dec3ce6b79b41ae8ed32a8
wintersoldie <73120282+wintersoldie@users.noreply.github.com>
2020-10-20 11:17:31 +0530

    Create code.c

---
+++ code.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,t,c,noc,k,k1,k2,i,j,count;
+    scanf("%d\n",&t);
+    for(j=0;j<t;j++)
+    {
+    scanf("%d %d %d",&n,&c,&m);
+    noc=n/c;
+    count=noc;
+
+    for(i=0;;)
+    {
+        if(noc>=m)
+        {
